c8a0ad3c75d4097626076534f337830b3602a4d2
FabiKel <fabi.kelbassa@gmail.com>
2025-09-22 12:13:31 +0200

    - forgot to build...

--- index.js
+++ index.js
@@ -12,12 +12,33 @@ jQuery(() => {
                     <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
                 </div>
                 <div class="inline-drawer-content">
-                    <p>Character Sheet Extension loaded! Version 0.1.0</p>
+                    <p>Character Sheet Extension loaded! Version 0.1.1</p>
                 </div>
             </div>
         </div>
     `;
     $("#extensions_settings").append(settingsHtml);
+    // UI Toolbar erstellen
+    createToolbar();
     console.log("[Character Sheet] Extension loaded successfully!");
 });
+function createToolbar() {
+    // Toolbar HTML
+    const toolbarHtml = `
+        <div id="char-sheet-toolbar" class="char-sheet-toolbar">
+            <button id="cs-inventory-btn" class="menu_button menu_button_icon">
+                <i class="fa-solid fa-backpack"></i>
+                <span>Inventar</span>
+            </button>
+        </div>
+    `;
+    // Einfügen über dem Textarea
+    const sendTextarea = $("#send_textarea");
+    const parentContainer = sendTextarea.parent();
+    parentContainer.before(toolbarHtml);
+    // Event Handler
+    $("#cs-inventory-btn").on("click", () => {
+        console.log("Inventar Button clicked!");
+    });
+}
 //# sourceMappingURL=index.js.map
--- types.js
+++ types.js
@@ -1,2 +1,18 @@
-"use strict";
+// Mögliche Ausrüstungsslots
+export const EQUIPMENT_SLOT_SUGGESTIONS = [
+    'head',
+    'neck',
+    'shoulders',
+    'chest',
+    'hands',
+    'ring_left',
+    'ring_right',
+    'waist',
+    'legs',
+    'feet',
+    'main_hand',
+    'off_hand',
+    'back',
+    'belt',
+]; // Für Beutel
 //# sourceMappingURL=types.js.map
